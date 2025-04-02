0fcd3e70a529d7f723ce345ccdf8eccd09285690
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-04-02 15:50:33 +0530

    Update data_register.py

--- data_science/src/data_register.py
+++ data_science/src/data_register.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="../data/teleco_churn.csv",
+    folder_path="../data",
     repo_id="praneeth232/test",
     repo_type="dataset",
 )
