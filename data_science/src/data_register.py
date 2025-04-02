974d2dbe62492876f5092db6c721e1f553115b87
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-04-02 15:44:43 +0530

    Update data_register.py

--- data_science/src/data_register.py
+++ data_science/src/data_register.py
@@ -1,25 +1,25 @@
-# from huggingface_hub import HfApi
-
-# api = HfApi(token=os.getenv("HF_TOKEN"))
-# api.upload_folder(
-#     folder_path="../data/teleco_churn.csv",
-#     repo_id="praneeth232/test",
-#     repo_type="dataset",
-# )
-
-
-from datasets import Dataset
 from huggingface_hub import HfApi
-import os
 
-# Define variables
-HF_TOKEN = os.getenv("HF_TOKEN")
-DATASET_REPO = "praneeth232/test"
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="../data/teleco_churn.csv",
+    repo_id="praneeth232/test",
+    repo_type="dataset",
+)
 
-# Load dataset from CSV
-dataset = Dataset.from_csv("data.csv")
 
-# Push dataset to Hugging Face Hub
-dataset.push_to_hub(DATASET_REPO, token=HF_TOKEN)
+# from datasets import Dataset
+# from huggingface_hub import HfApi
+# import os
 
-print(f"Dataset uploaded to: https://huggingface.co/datasets/{DATASET_REPO}")
+# # Define variables
+# HF_TOKEN = os.getenv("HF_TOKEN")
+# DATASET_REPO = "praneeth232/test"
+
+# # Load dataset from CSV
+# dataset = Dataset.from_csv("data.csv")
+
+# # Push dataset to Hugging Face Hub
+# dataset.push_to_hub(DATASET_REPO, token=HF_TOKEN)
+
+# print(f"Dataset uploaded to: https://huggingface.co/datasets/{DATASET_REPO}")
