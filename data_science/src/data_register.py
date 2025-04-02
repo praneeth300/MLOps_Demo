50d435e71131607af4e55e902435593525471529
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-04-02 16:02:13 +0530

    Update data_register.py

--- data_science/src/data_register.py
+++ data_science/src/data_register.py
@@ -7,20 +7,3 @@ api.upload_folder(
     repo_id="praneeth232/test",
     repo_type="dataset",
 )
-
-
-# from datasets import Dataset
-# from huggingface_hub import HfApi
-# import os
-
-# # Define variables
-# HF_TOKEN = os.getenv("HF_TOKEN")
-# DATASET_REPO = "praneeth232/test"
-
-# # Load dataset from CSV
-# dataset = Dataset.from_csv("data.csv")
-
-# # Push dataset to Hugging Face Hub
-# dataset.push_to_hub(DATASET_REPO, token=HF_TOKEN)
-
-# print(f"Dataset uploaded to: https://huggingface.co/datasets/{DATASET_REPO}")
