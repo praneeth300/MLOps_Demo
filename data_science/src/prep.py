1fdf513c82118b6f3fc2e7364a1836591de6297c
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-04-29 16:57:34 +0530

    Update prep.py

--- data_science/src/prep.py
+++ data_science/src/prep.py
@@ -1,19 +1,10 @@
 # for data manipulation
 import pandas as pd
 import sklearn
-# for data preprocessing and pipeline creation
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import make_column_transformer
-from sklearn.pipeline import make_pipeline
-# for model training, tuning, and evaluation
-import xgboost as xgb
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, classification_report, recall_score
-# for model serialization
-import joblib
 # for creating a folder
 import os
+# for data preprocessing and pipeline creation
+from sklearn.model_selection import train_test_split
 # for hugging face space authentication to upload files
 from huggingface_hub import login, HfApi
 
