c4f829faafc1bf00552741f814622b8921d2ef0b
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-05-06 16:39:10 +0530

    Update backend_deployment.py

--- data_science/src/deployment/backend_deployment.py
+++ data_science/src/deployment/backend_deployment.py
@@ -5,7 +5,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="backend",     # the local folder containing your files
-    repo_id="praneeth232/backend",          # the target repo
+    repo_id="praneeth232/backn-demo",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
